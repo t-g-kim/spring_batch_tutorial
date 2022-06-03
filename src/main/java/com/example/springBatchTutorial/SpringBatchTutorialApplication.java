326d280ed8a3c3be431f32df40e58c7b222ae540
t-g-kim <je23ct@gmail.com>
2022-06-03 19:30:11 +0900

    add helloworld job

--- src/main/java/com/example/springBatchTutorial/SpringBatchTutorialApplication.java
+++ src/main/java/com/example/springBatchTutorial/SpringBatchTutorialApplication.java
@@ -1,9 +1,12 @@
 package com.example.springBatchTutorial;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@EnableBatchProcessing
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class SpringBatchTutorialApplication {
 
 	public static void main(String[] args) {
